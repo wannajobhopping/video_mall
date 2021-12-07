84c22d66179332674db246d7bf67fd05e2bdf643
Eason <acontentedmind@gmail.com>
2021-12-07 20:00:18 +0800

    opt-設定必填欄位

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true, format: { with: /[\w]+@([\w-]+\.)+[\w-]{2,4}/ }
+  validates :password, presence: true, confirmation: true, length: { minimum: 4 }
+  validates :name, presence: true
   before_create :encrypt_password
-
 
   
 

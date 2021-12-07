a554f0824bae7d8bd02d4426247a4e1088cb4f87
Eason <acontentedmind@gmail.com>
2021-12-07 19:58:12 +0800

    opt-user建立前會先加密

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  before_create :encrypt_password
+
+
+  
+
+  private
+
+  def encrypt_password
+    self.passowrd = Digest::SHA1.hexdigest("qidftmw#{self.password}mdsfjiw")
+  end
 end

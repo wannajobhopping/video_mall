43d515c44e3d6061b8766965849ba79cb61b5ca4
Eason <acontentedmind@gmail.com>
2021-12-07 19:54:29 +0800

    feat-可創建user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,30 @@
 class UsersController < ApplicationController
   def index; end
-  def create; end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to(root_path)
+    else
+      render :new
+    end
+  end
+
   def edit; end
+
   def update; end
+
   def sign_in; end
+
   def sign_out; end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name)
+  end
 end
